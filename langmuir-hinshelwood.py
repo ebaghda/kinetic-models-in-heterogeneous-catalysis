c582c25ebe09bf23a3f51d70733988f1da27fb1d
ebaghda <ezrabaghdady@gmail.com>
2025-11-29 18:32:03 -0500

    improve model and plotting

--- langmuir-hinshelwood.py
+++ langmuir-hinshelwood.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
 def langmuir_hinshelwood_model(p_A, p_B, k_1, K_A, K_B):
-    return k_1 * K_A *p_A * K_B *p_B / (1 + K_A * p_A + K_B * p_B)**2
+    return k_1 * K_A * p_A * K_B * p_B / (1 + K_A * p_A + K_B * p_B)**2
 
 def main():
-    
-    p_A = np.ones(100)*20
+    colormap = matplotlib.colormaps['inferno']  #'viridis', 'plasma', 'inferno', 'coolwarm')
+    n_curves = 50
+    n_points = 150
+    p_A = np.ones(n_points)*10
+    K_A = np.ones(n_points)*0.6
     K_As = {}
     rates = []
     rate_strings = []
-    for K_A in np.linspace(0.001, 100, 100):
-        p_B = np.linspace(0, 10, 100)
-        plt.plot(p_B, langmuir_hinshelwood_model(p_A, p_B, 0.01, K_A=K_A, K_B=0.6))
-        K_As[K_A] = langmuir_hinshelwood_model(p_A, p_B, 0.01, K_A=K_A, K_B=0.6)
-        rates.append(langmuir_hinshelwood_model(p_A, p_B, 0.01, K_A=K_A, K_B=0.6))
+    for K_A in np.linspace(0.01, 10, n_curves): #loop over equilibrium constant for species A
+        p_B = np.linspace(0, 200, n_points) # pressure of B is the x-axis
+        plt.plot(p_B, langmuir_hinshelwood_model(p_A, p_B, k_1=0.05, K_A=K_A, K_B=0.6), 
+            linewidth=1.75, color = colormap(K_A / n_curves*5))
+        rates.append(langmuir_hinshelwood_model(p_A, p_B, k_1=0.05, K_A=K_A, K_B=0.6))
+    print(rates[0][0:5:-1])
+    print(rate_strings[0:4])
 
-
-    # format the plot
-    plt.xlabel("Pressure of B (kPa)")
-    plt.ylabel("Rate (mol/s) \n (k_1 = 0.01 mol/s·kPa²) \n (p_A = 20 kPa) \n (K_B = 0.6) \n (K_A = 0.05 - 200)")
-    plt.title("Langmuir-Hinshelwood Model")
-    # plt.legend(title="K_A", loc="upper left")
-    # plt.legend(K_As, title="K_A")
+    # format the plot with bold Arial font
+    font = {'weight': 'bold', 'fontsize':16}
+    plt.xlabel("Pressure of B (kPa)", fontdict=font)
+    plt.ylabel("Rate (mol/s)", fontdict=font)
+    plt.title("Langmuir-Hinshelwood Model", fontdict=font)
+    plt.xticks(fontname=None, fontweight='bold', fontsize=12)
+    plt.yticks(fontname=None, fontweight='bold', fontsize=12)
     plt.tight_layout()
     plt.savefig("langmuir-hinshelwood.png")
     print("Saved figure to langmuir-hinshelwood.png")
